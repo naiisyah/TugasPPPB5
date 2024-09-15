fa594d00501181835fc34decd81f49f6b79f7844
naiisyah <syahlanaiimah19@gmail.com>
2024-09-15 21:58:24 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tugaspppb5/HomeActivity.kt
@@ -0,0 +1,31 @@
+package com.example.tugaspertemuan5
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.tugaspppb5.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_home)
+        val tvWelcome = findViewById<TextView>(R.id.tvWelcome)
+        val tvEmail = findViewById<TextView>(R.id.tvEmail)
+        val tvPhone = findViewById<TextView>(R.id.tvPhone)
+
+        // Ambil data dari SharedPreferences
+        val sharedPreferences = getSharedPreferences("user_data", MODE_PRIVATE)
+        val username = sharedPreferences.getString("username", "")
+        val email = sharedPreferences.getString("email", "")
+        val phone = sharedPreferences.getString("phone", "")
+
+        // Tampilkan data
+        tvWelcome.text = "Welcome $username"
+        tvEmail.text = "Your email $email has been activated"
+        tvPhone.text = "Your phone $phone has been registered"
+    }
+}
